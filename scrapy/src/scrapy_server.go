f90683d564f4ec1607fd8371266b1d68f0b235d3
shin-dy <shindo@bizdev-agent.com>
2022-05-04 15:40:47 +0900

    add. DeployConfigur for GoogleAppEngine

---
+++ scrapy/src/scrapy_server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gocolly/colly"
+	"log"
+	"net/http"
+	"os"
+)
+
+type pageInfo struct {
+	StatusCode int
+	Links      map[string]int
+}
+
+func main() {
+	// example usage: curl -s 'http://127.0.0.1:7171/?url=http://go-colly.org/'
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "7171"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Printf("Open http://localhost:%s in the browser", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	URL := r.URL.Query().Get("url")
+	if URL == "" {
+		log.Println("missing URL argument")
+		return
+	}
+	log.Println("visiting", URL)
+
+	c := colly.NewCollector()
+
+	p := &pageInfo{Links: make(map[string]int)}
+
+	// count links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		if link != "" {
+			p.Links[link]++
+		}
+	})
+
+	// extract status code
+	c.OnResponse(func(r *colly.Response) {
+		log.Println("response received", r.StatusCode)
+		p.StatusCode = r.StatusCode
+	})
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("error:", r.StatusCode, err)
+		p.StatusCode = r.StatusCode
+	})
+
+	c.Visit(URL)
+
+	// dump results
+	b, err := json.Marshal(p)
+	if err != nil {
+		log.Println("failed to serialize response:", err)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(b)
+}
